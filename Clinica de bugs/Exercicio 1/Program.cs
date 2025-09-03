f864066d697ddbf46f3c4dbe872a845df817c9d0
luciemenpiazer <luciemenpiazer@alunos.utfpr.edu.br>
2025-09-02 21:13:49 -0300

    Exercicio 1
    Implementação do código do primeiro exercício proposto.

--- Clinica de bugs/Exercicio 1/Program.cs
+++ Clinica de bugs/Exercicio 1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ Console.WriteLine("Por favor, digite seu primeiro nome:")
+ string primeiroNome = Console.ReadLine();
+ Console.WriteLine("Agora, digite sua idade:");
+ int idade = Console.ReadLine();
+ Console.WriteLine("Por fim, digite sua altura em metros (ex: 1,80):");
+ float altura = float.Parse(Console.ReadLine());
+ Console.writeline($"Resumo do Cadastro: Nome: {PrimeiroNome}, Idade: {idade} 
+anos, Altura: {altura}m.")
