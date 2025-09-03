4e110d1a65a5966a5d78362b613bad67a7112c77
luciemenpiazer <luciemenpiazer@alunos.utfpr.edu.br>
2025-09-02 21:49:17 -0300

    Exercício 9
    Criação de pasta e projeto pelo console. Implementação do código proposto pelo exercício.

---
+++ Clinica de bugs/Exercicio9/Program.cs
@@ -0,0 +1,46 @@
+﻿ int[][] matriz = new int[3,3];
+ int somaTotal = 0
+ int somaDiagonal = 0;
+ int[] maiores = new int[3];
+ for (int i = 0; i <= 3; i++)
+ {
+ for (int j = 0; j <= 3; j++)
+ {
+ Console.Write($"Valor [{i},{j}]: ");
+ int valor = int.Parse(Console.ReadLine());
+ matriz[j, i] = valor;
+ somaTotal = somaTotal + valor;
+ if (i == j)
+ {
+ somaDiagonal += matriz[i, i + 1];
+ }
+ if (valor > maiores[i])
+ {
+ maiores[i] = valor;
+ }
+ }
+ }
+ Console.WriteLine("Soma total: {somaTotal}");
+ Console.WriteLine("Soma diagonal principal: " + somaDiagonal);
+ Console.WriteLine("Maiores por linha:");
+ for (int i = 0; i < 3; i++)
+ {
+ Console.WriteLine(maiores[j]);
+ }
+ int[,] transposta = new int[3][3];
+ for (int i = 0; i < 3; i++)
+ {
+ for (int j = 0; j < 3; j++)
+ {
+ transposta[i, j] = matriz[i, j];
+ }
+ }
+ Console.WriteLine("Matriz transposta:");
+ for (int i = 0; i < 3; i++)
+ {
+ for (int j = 0; j < 3; j++)
+ {
+ Console.Write(transposta[i,j] + " ");
+ }
+ Console.WriteLine()
+ }
