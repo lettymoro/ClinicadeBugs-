dc63d8f1b579a79d36d25cb0db6960093370a2c4
luciemenpiazer <luciemenpiazer@alunos.utfpr.edu.br>
2025-09-02 21:20:15 -0300

    Exercicio 2
    Criação de pasta e arquivo pelo console e implementação do código para o segundo exercício proposto.

---
+++ Clinica de bugs/Exercicio2/Program.cs
@@ -0,0 +1,11 @@
+﻿float notaProva = 8.5;
+ int quantidadeAlunos = "42";
+ string nomeCurso = 'Fundamentos de C
+ #';
+ bool pagamentoEfetuado = "false";
+ char digitoVerificador = "7"
+ Console.WriteLine(notaProva);
+ Console.WriteLine(quantidadeAlunos);
+ Console.WriteLine(nomecurso);
+ Console.WriteLine(pagamentoEfetuado);
+ Console.WriteLine(digitoVerificador);
