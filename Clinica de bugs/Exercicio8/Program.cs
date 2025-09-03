6a23762cc869c56772e7c8f6ecce5a86c54abfc7
luciemenpiazer <luciemenpiazer@alunos.utfpr.edu.br>
2025-09-02 21:46:29 -0300

    Exercicio 8
    Criação de pasta e projeto pelo console. Implementação do código proposto pelo exercício.

---
+++ Clinica de bugs/Exercicio8/Program.cs
@@ -0,0 +1,11 @@
+﻿ int[] notas = new int[5];
+ for (int i = 0; i <= notas.Length; i++)
+ {
+ Console.Write($"Digite a nota {i + 1}: ");
+ notas[i] = int.Parse(Console.ReadLine());
+ }
+ for (int i = 0; i < notas.Length; i++)
+ {
+ string situacao = notas[i] = 7 ? "Aprovado" : "Reprovado";
+ Console.WriteLine($"Nota {i + 1}: {notas[i]} - {situacao}");
+ }
