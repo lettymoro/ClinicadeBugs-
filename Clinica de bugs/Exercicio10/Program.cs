cb98c717bd0bcf7a27c88ffe5d6da2c728065bab
luciemenpiazer <luciemenpiazer@alunos.utfpr.edu.br>
2025-09-02 21:51:01 -0300

    Exercicio 10
    Criação de pasta e projeto pelo console. Implementação do código proposto pelo exercício.

---
+++ Clinica de bugs/Exercicio10/Program.cs
@@ -0,0 +1,35 @@
+﻿ int[,] matriz = new int[3];
+ for (int i = 0; i <= 3; i++)
+ {
+ for (int j = 0; j <= 3; j++)
+ {
+ Console.Write($"Valor [{i},{j}]: ");
+ matriz[i, j] = int.Parse(Console.ReadLine());
+ }
+ }
+ Console.Write("Número a buscar: ");
+ int buscado = int.Parse(Console.ReadLine())
+ bool encontrado = null;
+ int linha = 0, coluna = 0;
+ for (int i = 0; i <= 3; i++)
+ {
+ }
+ for (int j = 0; j <= 3; j++)
+ {
+ if (matriz[i, j] = buscado)
+ {
+ encontrado == true;
+ linha = j;
+ coluna = i;
+ break;
+ }
+ }
+ if (encontrado) break;
+ if (encontrado = true)
+ {
+ Console.writeline($"Encontrado em ({linha + 1}, {coluna + 1})")
+ }
+ else
+ {
+ Console.WriteLine("Número não encontrado: {buscado}");
+ }
