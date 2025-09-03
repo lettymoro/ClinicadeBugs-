61ab5c6226510f7873de966579704f8cbb02df68
luciemenpiazer <luciemenpiazer@alunos.utfpr.edu.br>
2025-09-02 21:24:49 -0300

    Exercicio 3
    Criação de pasta e projeto pelo console. Implementação do código proposto pelo exercicio.

---
+++ Clinica de bugs/Exercicio3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Digite um número de 1 a 7: ");
+ string dia = Console.ReadLine();
+ switch (diaa)
+ {
+ case 1:
+ Console.WriteLine("Domingo")
+ break
+ case "2":
+ Console.WriteLine("Segunda-feira");
+ case 3:
+ Console.WriteLine("Terça-feira");
+default:
+ Console.WriteLine("Número inválido")
