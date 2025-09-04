7d2454c8cdd27ecfb6c0421acedac0aec0d83d47
luciemenpiazer <luciemenpiazer@alunos.utfpr.edu.br>
2025-09-04 10:24:48 -0300

    Correção do Exercicio 2
    Correção de código e implementação de comentários dos erros referente ao Exercício2
    
    Co-Authored-By: lettymoro <229150573+lettymoro@users.noreply.github.com>

--- Clinica de bugs/Exercicio2/Program.cs
+++ Clinica de bugs/Exercicio2/Program.cs
@@ -1,11 +1,12 @@
-﻿float notaProva = 8.5;
- int quantidadeAlunos = "42";
- string nomeCurso = 'Fundamentos de C
- #';
- bool pagamentoEfetuado = "false";
- char digitoVerificador = "7"
+using System;
+
+ Double notaProva = 8.5; //(E1) Tipo de dado incorreto (CS0029)
+ int quantidadeAlunos = 42; //(E2) valor de int em aspas dupla(CS0029)
+ string nomeCurso = "Fundamentos de C#"; //(E3) valor de string em aspas simples(CS0029)
+ bool pagamentoEfetuado = false; //(E4) Valor booleano em aspas duplas(CS0029)
+ char digitoVerificador = '7'; //(E5) Valor de char em aspas simples e falta de ; (CS1002)
  Console.WriteLine(notaProva);
  Console.WriteLine(quantidadeAlunos);
- Console.WriteLine(nomecurso);
+ Console.WriteLine(nomeCurso); //(E6) Nome de variavel invalida, c# não é case sensitive (CS0103)
  Console.WriteLine(pagamentoEfetuado);
  Console.WriteLine(digitoVerificador);
