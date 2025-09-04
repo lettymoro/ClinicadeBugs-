99b2d51b2e6e2698244fa76317640175debb10bc
luciemenpiazer <luciemenpiazer@alunos.utfpr.edu.br>
2025-09-04 09:52:10 -0300

    Correção Exercicio 1
    Correção de código e implementação de comentários referente ao Exercicio 1
    
    Co-Authored-By: lettymoro <229150573+lettymoro@users.noreply.github.com>

--- Clinica de bugs/Exercicio1/Program.cs
+++ Clinica de bugs/Exercicio1/Program.cs
@@ -1,8 +1,12 @@
-﻿ Console.WriteLine("Por favor, digite seu primeiro nome:")
- string primeiroNome = Console.ReadLine();
- Console.WriteLine("Agora, digite sua idade:");
- int idade = Console.ReadLine();
- Console.WriteLine("Por fim, digite sua altura em metros (ex: 1,80):");
- float altura = float.Parse(Console.ReadLine());
- Console.writeline($"Resumo do Cadastro: Nome: {PrimeiroNome}, Idade: {idade} 
-anos, Altura: {altura}m.")
+﻿ using System;
+ using System.Globalization;
+
+ Console.Write("Por favor, digite seu primeiro nome: ");
+ string primeiroNome = Console.ReadLine() ?? string.Empty; //(E1) ; esperado (CS1002)
+ Console.Write("Agora, digite sua idade: ");
+ int idade = int.Parse(Console.ReadLine()!); // (E2) Não é possível converter implicitamente o tipo 'string' em 'int (CS0029)
+ Console.Write("Por fim, digite sua altura em metros (ex: 1.80): ");
+ float altura = float.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture); // (E3) Adicionamos CultureInfo.InvariantCulture 
+                                                                                // para usar ponto como separador decimal. (CS0103)
+ Console.WriteLine($"Resumo do Cadastro: Nome: {primeiroNome}, Idade: {idade} anos, Altura: {altura.ToString("0.00", CultureInfo.InvariantCulture)} m.");
+ //(E4 Linha 8) 'PrimeiroNome' não existe no contexto atual (CS0103)
